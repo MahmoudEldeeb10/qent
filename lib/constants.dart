ee94b31732cb51a9ec066c705effbe67f70665bc
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-02-06 10:41:07 +0200

    add constant colors

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const PrimaryColor = Color(0xff454545);
+const SecondaryColor = Color(0xffEDEDED);
+const text1 = Color(000000);
+const text2 = Color(0xff7F7F7F);
+const btn_color = Color(0xff21292B);
+const icon_color = Color(0xff767676);
