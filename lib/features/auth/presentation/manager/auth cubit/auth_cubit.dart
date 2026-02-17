e95c59d1a7054d0324269876d36285622e200f06
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-02-17 21:41:11 +0200

    add auth cubit

---
+++ lib/features/auth/presentation/manager/auth cubit/auth_cubit.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qent/features/auth/data/services/auth_service.dart';
+import 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  final AuthService _authService;
+
+  AuthCubit(this._authService) : super(AuthInitial());
+
+  //  Login
+  Future<void> login({required String email, required String password}) async {
+    emit(AuthLoading());
+    try {
+      final response = await _authService.login(
+        email: email,
+        password: password,
+      );
+      emit(AuthSuccess(response));
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
+  //  Register
+  Future<void> register({
+    required String fullName,
+    required String email,
+    required String phone,
+    required String password,
+    int country = 1,
+    int location = 6,
+  }) async {
+    emit(AuthLoading());
+    try {
+      final response = await _authService.register(
+        fullName: fullName,
+        email: email,
+        phone: phone,
+        password: password,
+        country: country,
+        location: location,
+      );
+      emit(AuthSuccess(response));
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void resetState() => emit(AuthInitial());
+}
