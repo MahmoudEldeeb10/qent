12ed77d1fd82c5b10f46c3c6fcfd7e73f23e7026
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-03-23 14:33:25 +0200

    remove comments in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,10 +9,3 @@ class AppColors {
   static const Color icon_color = Color(0xff767676);
   static const Color cars_background = Color(0xffF0F0F0);
 }
-
-// const PrimaryColor = Color(0xff454545);
-// const SecondaryColor = Color(0xffEDEDED);
-// const text1 = Color(0x00000000);
-// const text2 = Color(0xff7F7F7F);
-// const btn_color = Color(0xff21292B);
-// const icon_color = Color(0xff767676);
