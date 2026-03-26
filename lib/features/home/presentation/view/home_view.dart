b238f33b50e40f2d6e4608e865a3e77a91529908
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-03-26 12:30:14 +0200

    edit home appbar

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -3,7 +3,7 @@ import 'package:qent/features/home/presentation/widgets/brand_section.dart';
 import 'package:qent/features/home/presentation/widgets/car_card_gridview.dart';
 import 'package:qent/features/home/presentation/widgets/home_app_bar.dart';
 import 'package:qent/features/home/presentation/widgets/nearby_section.dart';
-import 'package:qent/features/home/presentation/widgets/search_section.dart';
+import 'package:qent/core/widgets/search_section.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,22 +11,20 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: const HomeAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              HomeAppBar(),
-              Divider(color: Colors.grey, thickness: 1),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               SearchSection(),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               BrandsSection(),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               CarCardGridView(),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               NearBySection(),
             ],
           ),
--- lib/features/home/presentation/widgets/home_app_bar.dart
+++ lib/features/home/presentation/widgets/home_app_bar.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:qent/constants.dart';
 
-class HomeAppBar extends StatelessWidget {
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset('assets/images/black_logo.png', height: 90, width: 90),
-        Spacer(),
-        IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
-        CircleAvatar(
-          radius: 12,
-          backgroundImage: AssetImage('assets/images/profile.png'),
+    return AppBar(
+      leadingWidth: 100,
+      toolbarHeight: 80,
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('assets/images/black_logo.png', fit: BoxFit.contain),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+          child: IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
+          child: CircleAvatar(
+            radius: 12,
+            backgroundImage: AssetImage('assets/images/profile.png'),
+          ),
         ),
       ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(1.0),
+        child: Container(color: AppColors.stoke, height: 1.0),
+      ),
     );
   }
 }
